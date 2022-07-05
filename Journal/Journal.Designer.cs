8f077bb7f717df4cf7b7ca32ccfdae6b750a2873
20erik03 20erik03 <margiev_e@mail.ru>
2022-07-05 23:58:29 +0300

    Добавьте файлы проекта.

---
+++ Journal/Journal.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Journal
+{
+    partial class FormJournal
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDateDelete = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDateAdd = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.comBoxDiscipline = new System.Windows.Forms.ComboBox();
+            this.comBoxMonth = new System.Windows.Forms.ComboBox();
+            this.DGVJournal = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVJournal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonSave);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxDateDelete);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonDelete);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxDateAdd);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonAdd);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonShow);
+            this.splitContainer1.Panel1.Controls.Add(this.comBoxDiscipline);
+            this.splitContainer1.Panel1.Controls.Add(this.comBoxMonth);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.DGVJournal);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 200;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(61, 404);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Удалить столбец";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Введите дату:";
+            // 
+            // textBoxDateDelete
+            // 
+            this.textBoxDateDelete.Location = new System.Drawing.Point(95, 323);
+            this.textBoxDateDelete.Name = "textBoxDateDelete";
+            this.textBoxDateDelete.Size = new System.Drawing.Size(91, 20);
+            this.textBoxDateDelete.TabIndex = 7;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(61, 349);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 224);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Добавить столбец";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите дату:";
+            // 
+            // textBoxDateAdd
+            // 
+            this.textBoxDateAdd.Location = new System.Drawing.Point(95, 240);
+            this.textBoxDateAdd.Name = "textBoxDateAdd";
+            this.textBoxDateAdd.Size = new System.Drawing.Size(91, 20);
+            this.textBoxDateAdd.TabIndex = 3;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(61, 266);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(61, 137);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(75, 23);
+            this.buttonShow.TabIndex = 2;
+            this.buttonShow.Text = "Вывести";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // comBoxDiscipline
+            // 
+            this.comBoxDiscipline.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comBoxDiscipline.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comBoxDiscipline.FormattingEnabled = true;
+            this.comBoxDiscipline.Location = new System.Drawing.Point(12, 110);
+            this.comBoxDiscipline.Name = "comBoxDiscipline";
+            this.comBoxDiscipline.Size = new System.Drawing.Size(174, 21);
+            this.comBoxDiscipline.TabIndex = 1;
+            // 
+            // comBoxMonth
+            // 
+            this.comBoxMonth.AutoCompleteCustomSource.AddRange(new string[] {
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+            "Январь",
+            "Февраль",
+            "Март ",
+            "Апрель ",
+            "Май ",
+            "Июнь",
+            "Июль"});
+            this.comBoxMonth.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comBoxMonth.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comBoxMonth.FormattingEnabled = true;
+            this.comBoxMonth.Items.AddRange(new object[] {
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь",
+            "Январь",
+            "Февраль",
+            "Март ",
+            "Апрель ",
+            "Май ",
+            "Июнь",
+            "Июль"});
+            this.comBoxMonth.Location = new System.Drawing.Point(12, 73);
+            this.comBoxMonth.Name = "comBoxMonth";
+            this.comBoxMonth.Size = new System.Drawing.Size(174, 21);
+            this.comBoxMonth.TabIndex = 0;
+            // 
+            // DGVJournal
+            // 
+            this.DGVJournal.AllowUserToAddRows = false;
+            this.DGVJournal.AllowUserToOrderColumns = true;
+            this.DGVJournal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVJournal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVJournal.Location = new System.Drawing.Point(0, 0);
+            this.DGVJournal.Name = "DGVJournal";
+            this.DGVJournal.RowHeadersVisible = false;
+            this.DGVJournal.Size = new System.Drawing.Size(596, 450);
+            this.DGVJournal.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Выберите месяц";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Выберите предмет";
+            // 
+            // FormJournal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormJournal";
+            this.Text = "Журнал";
+            this.Load += new System.EventHandler(this.FormJournal_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVJournal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.ComboBox comBoxDiscipline;
+        private System.Windows.Forms.ComboBox comBoxMonth;
+        private System.Windows.Forms.DataGridView DGVJournal;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDateAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxDateDelete;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Journal/Journal.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Journal
+{
+    public partial class FormJournal : Form
+    {
+        //Флаг выбранна ли дисцыплина
+        bool disciplinSelected = false;
+
+        public FormJournal()
+        {
+            InitializeComponent();
+        }
+
+        //Закгрузка окна
+        private void FormJournal_Load(object sender, EventArgs e)
+        {
+            //Зполнение комбовоксов
+            StreamReader sr = new StreamReader("Disciplines.txt");//открыть файл с дисциплинами
+            var s = sr.ReadToEnd().Trim('\n').Split('\n');//считать весь файл очитстить от лишнего
+            comBoxDiscipline.Items.AddRange(s);//добавить все дисциплины в колекцию выплыв списка
+            comBoxDiscipline.AutoCompleteCustomSource.AddRange(s);//добавить все дисциплины в колекцию
+                                                                  //автодобавления выплыв списка
+
+            //Заолнение ДатаГридВью
+            sr = new StreamReader("Students.txt");//открыть файл со студентами
+            s = sr.ReadToEnd().Trim('\n').Split('\n');//считать весь файл очитстить от лишнего
+            DGVJournal.Columns.Add("Names", "Студенты");//добавить колонку студентов
+            for (int i = 0; i < s.Length; i++)
+            {
+                DGVJournal.Rows.Add(s[i]);//добавить строку с именем студента
+            }
+            sr.Close();//закрыть файл
+        }
+
+        //Очистка таблицы
+        private void ClearDGV() 
+        {
+            var columnsCount = DGVJournal.Columns.Count;//количество столбцов
+            for (int i = 1; i < columnsCount; i++)
+                DGVJournal.Columns.RemoveAt(1);//удалить первый столбец
+        }
+
+        //Конпка вывести
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            ClearDGV();
+            var month = comBoxMonth.Text;//считать месяц
+            var dis = comBoxDiscipline.Text.Trim('\r').Trim();//считать дисциплину и очистить от лтшнего
+            if (String.IsNullOrEmpty(month))//если месяц не выбран
+                MessageBox.Show("Вы не выбрали месяц");
+            else if (String.IsNullOrEmpty(dis))//если дисциплина не выбрана
+                MessageBox.Show("Вы не выбрали дисциплину");
+            else
+            {
+                disciplinSelected = true;//пометить что дисциплина выбрана
+                StreamReader sr = new StreamReader($"Months\\{month}.txt");//перейти в папку                                                       //и открыть файл с выбраным месяцем
+                if (sr == null) MessageBox.Show("Вы неправильно ввели данные");//если месяц был введён неправильно
+                else 
+                {
+                    var s = sr.ReadLine();//считать строку
+                    while (!String.IsNullOrEmpty(s))//пока строка не пустая
+                    {
+                        if (s[0] == '$')//если строка начинается с доллара
+                        {
+                            var disTemp = s.Split('$');
+                            if (disTemp[1] == dis)//если дисциплина совпадает
+                            {
+                                var day = sr.ReadLine();//считать сроку с датой и оценками
+                                for (int i = 1; day[0] != '&'; i++)//пока не дойдём до &
+                                {
+                                    var dayTemp = day.Split('#');//разделить по #
+                                    var dayColumn = new DataGridViewTextBoxColumn();//новая колонка
+                                    dayColumn.Name = dayTemp[0];//имя столбца присвоить число
+                                    dayColumn.Width = DGVJournal.RowTemplate.Height;//установить ширину колонок
+                                    DGVJournal.Columns.Add(dayColumn);//добавить колонку в таблицу
+                                    for (int j = 0; j < dayTemp[1].Length; j++)
+                                    {
+                                        DGVJournal[i, j].Value = dayTemp[1][j] == '_' ? 
+                                            "" : dayTemp[1][j].ToString();//перенести оценки из файла в таблицу
+                                    }
+                                    day = sr.ReadLine();//считать след строку
+                                }                               
+                            }
+                        }
+                        s = sr.ReadLine();//считать след строку
+                    }
+                }
+                sr.Close();//закрыть файл
+            }
+        }
+
+        //кнопка добавлния
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var date = textBoxDateAdd.Text.Trim();//считать число которое надо добавить
+
+            if (!disciplinSelected)//если дисциплина не выбрана
+                MessageBox.Show("Вы не выбрали дисциплину");
+            else if (String.IsNullOrEmpty(date))//если поле пустое
+                MessageBox.Show("Вы не выбрали число");
+            else if (int.Parse(date) > 31 || double.Parse(date) % 1 != 0 || int.Parse(date) < 0)//если введена некоректная дата
+                MessageBox.Show("Некорректная дата");
+            else 
+            {
+                var dayColumn = new DataGridViewTextBoxColumn();//новая колонка
+                dayColumn.Name = date;//имя столбца присвоить число
+                dayColumn.Width = DGVJournal.RowTemplate.Height;//установить ширину колонок
+
+                var cols = new List<string>();//лист с колонками
+                var isAdded = false;//добавлена ли колонка
+                foreach (DataGridViewColumn col in DGVJournal.Columns)//пройтись по всем колокам
+                {
+                    if (col.Name != "Names")//если это не колонка с именами
+                    {
+                        if(Convert.ToInt32(col.Name) == Convert.ToInt32(dayColumn.Name))//если колонка с таким числом уже сущ
+                        {
+                            MessageBox.Show("Такая дата уже существует");
+                            textBoxDateAdd.Text = "";//очитстить текстовое поле
+                            return;
+                        }
+                        if (Convert.ToInt32(col.Name) > Convert.ToInt32(dayColumn.Name) && !isAdded)//если это правильное место для даты
+                                                                                                    //и она ещё не добавлена
+                        {
+                            isAdded = true;//пометить что колонку уже добавили
+                            cols.Add(dayColumn.Name);//добавить новую колонку
+                        }                       
+                        cols.Add(col.Name);//дубавить существующую колонку
+                    }
+                }
+                if (!isAdded)//если колонка ещё не была добавлена
+                {
+                    cols.Add(dayColumn.Name);//добавить новую колонку
+                }
+
+                int colsLength = DGVJournal.ColumnCount + 1, rowsLength = DGVJournal.RowCount;//пометить границы
+
+                var matrJournal = new string[colsLength, rowsLength];//матрица с оценками
+                for (int i = 1; i < colsLength-1; i++)
+                    for (int j = 0; j < rowsLength; j++)//пройтись по всей матрице
+                    {
+                        if (Convert.ToInt32(DGVJournal.Columns[i].Name) > Convert.ToInt32(dayColumn.Name))//полсе добавленной колонки 
+                            matrJournal[i + 1, j] = DGVJournal[i, j].Value == null ? null : DGVJournal[i, j].Value.ToString();
+                            //присвоить на одну колонку леве
+                        else
+                            matrJournal[i, j] = DGVJournal[i, j].Value == null ? null : DGVJournal[i, j].Value.ToString();
+                    }
+                ClearDGV();
+
+                for (int i = 1; i < colsLength; i++)
+                {
+                    var tempColumn = new DataGridViewTextBoxColumn();
+                    tempColumn.Name = cols[i - 1];
+                    tempColumn.Width = DGVJournal.RowTemplate.Height;
+                    DGVJournal.Columns.Add(tempColumn);//добавить колонку из листа
+                    for (int j = 0; j < rowsLength; j++)
+                        DGVJournal[i, j].Value = matrJournal[i, j] ?? "";//если значение null присвоить пустую строку
+                }
+            }
+            textBoxDateAdd.Text = "";//очистить текстовое поле добавить колонку
+        }
+
+        //Кнопка удалить
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            var date = textBoxDateDelete.Text.Trim();//считать с текстового поля удалить колоннку
+            if (!disciplinSelected)//если дисциплина не выбрана
+                MessageBox.Show("Вы не выбрали дисциплину");
+            else if (String.IsNullOrEmpty(date))//если поле пустое
+                MessageBox.Show("Вы не выбрали число");
+            else if (int.Parse(date) > 31 || double.Parse(date) % 1 != 0 || int.Parse(date) < 0)//если введена некоректная дата
+                MessageBox.Show("Некорректная дата");
+            else
+            {
+                for (int i = 1; i < DGVJournal.Columns.Count; i++)
+                {
+                    if (DGVJournal.Columns[i].Name == date)//если даты совпадают
+                    {
+                        DGVJournal.Columns.RemoveAt(i);//удалить колонку
+                    }
+                }
+                textBoxDateDelete.Text = "";//очистить поле удалить колонку
+            }
+        }
+
+        //кнопка сохранения
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (!disciplinSelected)//если дисциплина не выбрана
+                return;
+
+            int colsLength = DGVJournal.ColumnCount, rowsLength = DGVJournal.RowCount;//отметить границы
+            var dicJournal = new Dictionary<string, List<string>>();//словарь в который будет сохраняться информация
+
+            for (int i = 1; i < colsLength; i++)
+                for (int j = 0; j < rowsLength; j++)//пройтись по таблице
+                {
+                    if (!dicJournal.ContainsKey(DGVJournal.Columns[i].Name))//если нет такой даты
+                        dicJournal[DGVJournal.Columns[i].Name] = new List<string>();//создать лист
+                    dicJournal[DGVJournal.Columns[i].Name]
+                        .Add(DGVJournal[i, j].Value == null || String.IsNullOrEmpty(DGVJournal[i, j].Value.ToString()) ? "_" : DGVJournal[i, j].Value.ToString());   
+                    //добавить в лист значение из ячейки таблицы
+                }
+
+            var month = comBoxMonth.Text;//считать месяц
+            var dis = comBoxDiscipline.Text.Trim('\r').Trim();//считать дисциплину
+
+            StreamReader sr = new StreamReader($"Months\\{month}.txt");//открыть файл с выбраным месяцем
+
+            var fileStart = "";//часть файла до дисциплины, которую надо перезаписать
+            var newDis = "";//часть файла с новой инф о дисциплине
+            var fileEnd = "";//часть файла до дисциплины, которую надо перезаписать
+            var isStart = true;//если дисциплина ещё не изменена
+
+            if (sr == null) MessageBox.Show("Вы неправильно ввели данные");//если файл был неправильно выбран
+            else
+            {               
+                var s = sr.ReadLine();//считать строку
+                while (!String.IsNullOrEmpty(s))//пока строка не пустая
+                {
+                    if (s[0] == '$')//если строка начинается с доллара
+                    {
+                        var disTemp = s.Split('$');
+                        if (disTemp[1] == dis)//если дисциплина совпадает
+                        {
+                            newDis = $"${dis}$\n";//начало новой дисциплины
+                            isStart = false;//изменить на дисциплина уже изменена
+                            var day = sr.ReadLine();//считать строку
+                            while (day[0] != '&')//пока не дошёл до конца дисциплину
+                                day = sr.ReadLine();//считать строку
+                            foreach (var dayItem in dicJournal)//пройтись по словарю
+                            {
+                                var dayTemp = dayItem.Key.ToString() + "#" + String.Join("", dayItem.Value) + "\n";
+                                newDis += dayTemp;//добавить строку с числом 
+                            }
+                            newDis += $"&{dis}&\n";//конец новой дисциплины
+                            s = sr.ReadLine();//считать строку
+                        }
+                    }
+                    if (isStart)//до дисциплины 
+                        fileStart += s+"\n";//добавить строку
+                    else
+                        fileEnd += s+"\n";//добавить строку
+                    s = sr.ReadLine();//считать строку
+                }
+            }
+            sr.Close();//закрыть файл
+            File.WriteAllText($"Months\\{month}.txt", fileStart+newDis+fileEnd);//пререзаписать файл
+        }
+    }
+}
